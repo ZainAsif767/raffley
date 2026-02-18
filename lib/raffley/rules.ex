68e7b28862a0e9aaaf52505cbcd9811797618f69
Zain Asif <zainasif7345@gmail.com>
2026-02-18 14:00:12 +0500

    add rule controller and view with helper function

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,22 @@
+defmodule Raffley.Rules do
+  def list_rules do
+    [
+      %{
+        id: 1,
+        text: "Participant must have a high tolerance for puns and dad jokes."
+      },
+      %{
+        id: 2,
+        text: "Participants must be able to laugh at themselves and others."
+      },
+      %{
+        id: 3,
+        text: "Winner must do a victory dance when claiming their prize 🕺🏻."
+      },
+      %{
+        id: 4,
+        text: "Have Fun! 🤩"
+      }
+    ]
+  end
+end
